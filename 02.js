44b5c958d38234018f5c7339fc13bfc07d990c32
Ebniemann <78435724+Ebniemann@users.noreply.github.com>
2022-09-08 12:23:45 -0300

    Add files via upload

---
+++ 02.js
@@ -0,0 +1,14 @@
+function sumaTodos(array) {
+  // La funcion llamada 'sumaTodos' recibe como argumento un array con dos numeros
+  // y debe devolver la suma total entre todos los numeros dentro de ese rango
+  // ej:
+  // sumaTodos([1,3]) => 1 + 2 + 3 = 6
+  // Nota: Los numeros estan ordenados de menor a mayor.
+
+  // Tu código aca:
+  
+}
+
+// No modifiques nada debajo de esta linea //
+
+module.exports = sumaTodos
