548eaecf94be572dfcecf87fcf33b35e15c8cb48
Ebniemann <78435724+Ebniemann@users.noreply.github.com>
2022-09-08 12:18:56 -0300

    Add files via upload

---
+++ TEST/Ejercicios/01.js
@@ -0,0 +1,14 @@
+function soloNumeros(array) {
+  // La funcion llamada 'soloNumeros' recibe como argumento un arreglo de enteros y strings llamado 'array'.
+  // Debe devolver un arreglo con solo los enteros.
+  // Ej: 
+  // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
+
+  // Tu código aca:
+
+}
+
+// No modifiques nada debajo de esta linea //
+
+
+module.exports = soloNumeros
---
+++ TEST/Ejercicios/03.js
@@ -0,0 +1,25 @@
+function checkInventario(inventario, item) {
+  // La funcion 'checkInventario' recibe como argumento un array de objetos llamado 'inventario' y el nombre de un item llamado 'item'
+  // Cada objeto tiene una propiedad 'nombre' y 'cantidad'.
+  // La funcion debe devolver la cantidad de items que hay.
+  // Si el item no existe la funcion tiene que devolver 0 (cero).
+  // Ej:
+  // var inventario = [
+  //   {
+  //     nombre: 'tenedor',
+  //     cantidad: 6
+  //   },
+  //   {
+  //     nombre: 'cuchara',
+  //     cantidad: 4,
+  //   },
+  // ]
+  // checkInventario(inventario, 'tenedor') devuelve => 6
+
+  // Tu código aca:
+  
+}
+
+// No modifiques nada debajo de esta linea //
+
+module.exports = checkInventario
